b02200daf89337775082819acad07f67b0da58b9
s-deep3k <sankhadeep.genx@gmail.com>
2024-10-17 04:37:46 +0530

    Header done. Signup and signin to be done next

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 
 const Documents = ({params}:{params: {id:string}}) => {
   const id = params.id;
   return (
     <div>
+      <Header>
+        <div className="flex w-fit justify-center items-center">
+        <p className="document-title">This is a fake document title</p>
+        </div>
+      </Header>
       <Editor/>
     </div>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,16 +2,24 @@ import Image from "next/image"
 import Link from "next/link"
 
 
-const Header = () => {
+const Header = ({children}:HeaderProps) => {
   return (
-    <div>
-        <Link href=""/>
-        <Image src="/assets/logo.png" 
-        alt="Logo with name"
-        width={32}
-        height={32}
-        
-        />
+    <div className="header">
+        <Link href="/">
+          <Image src="/assets/icons/logo.svg" 
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="md:flex-1"
+          />
+          <Image src="/assets/icons/logo-icon.svg" 
+          alt="Logo with icon"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+          />
+        </Link>
+        {children}
     </div>
   )
 }
