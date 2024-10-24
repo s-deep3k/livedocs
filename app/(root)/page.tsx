aa6bea2ce6760dd0f19b8c81300f869d9d5b348e
s-deep3k <sankhadeep.genx@gmail.com>
2024-10-25 04:18:09 +0530

    server action created. Now making Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,30 @@
+import Header from "@/components/Header"
 import { Button } from "@/components/ui/button"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
 
-const page = () => {
+const Home = () => {
+  const documents =[]
   return (
-    <div>
-      <Button variant="default">Click Me</Button>
-    </div>
+   <main className="home-container">
+    <Header className="sticky left-0 top-0">
+      <div className="flex items-center gap-2 lg:gap-4">
+        Notification
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+      </div>
+    </Header>
+    {documents.length > 0 ?
+      <div></div>
+      :
+      <div className="document-list-empty">
+        <Image src={"/assets/icons/doc.svg"} alt="document" 
+        width={32} height={32} className=""/>
+      </div>
+    }
+   </main>
   )
 }
 
-export default page
+export default Home
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,7 +1,33 @@
+"use server";
+
+import { revalidatePath } from "next/cache"
 import { liveblocks } from "../liveblocks"
 import {nanoid} from 'nanoid'
-export const createDocument = ({email, userId}:CreateDocumentParams)=>{
-    // const room = liveblocks.createRoom(
-    //     nanoid(),
-    // )
+import { parseStringify } from "../utils";
+export const createDocument = async ({email, userId}:CreateDocumentParams)=>{
+    try {
+        const roomId = nanoid()
+        const metadata = {
+            creatorId: userId,
+            email,
+            title:'Untitled'
+        }
+        const usersAccesses:RoomAccesses = {
+            [email]:["room:write"]
+        }
+        const room = await liveblocks.createRoom(
+            roomId,{
+                metadata,
+                defaultAccesses:[],
+                usersAccesses
+            }
+        )
+        revalidatePath("/")
+
+        return parseStringify(room)
+    } catch (error) {
+        console.log(`Error happend while creating a room: ${error}`);
+        
+    }
+    
 }
