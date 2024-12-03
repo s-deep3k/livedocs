550ce873f3b91e06df34afce41317a784486cf2f
s-deep3k <sankhadeep.genx@gmail.com>
2024-12-04 03:06:14 +0530

    Listing all docs nicely on a page and opening them/ editing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn"
 import Header from "@/components/Header"
 import { getDocuments } from "@/lib/actions/room.actions"
+import { dateConverter } from "@/lib/utils"
 import { SignedIn, UserButton } from "@clerk/nextjs"
 import { currentUser } from "@clerk/nextjs/server"
 import Image from "next/image"
+import Link from "next/link"
 import { redirect } from "next/navigation"
 
 const Home = async () => {
@@ -26,7 +28,31 @@ const Home = async () => {
       <div className="document-list-container">
         <div className="document-list-title">
           <h3 className="text-28-semibold">All documents</h3>
+          <AddDocumentBtn
+          email={clerkUser.emailAddresses[0].emailAddress}
+          userId={clerkUser.id}
+          />
         </div>
+        <ul className="document-ul">
+        {
+          roomDocuments.data.map(({id,metadata, createdAt}:any)=>{
+            <li key={id} className="document-list-item">
+              <Link href={`/documents/${id}`} className="flex flex-1 gap-4 items-center" >
+                <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                  <Image src='/assets/icons/doc.svg' alt="file"
+                  width={40} height={40}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                  <p className="text-sm font-light text-blue-100"> Created about {dateConverter(createdAt)}</p>
+                </div>
+                </Link>
+                //TODO:delete button
+            </li>
+          })
+        }
+        </ul>
       </div>
       :
       <div className="document-list-empty">
